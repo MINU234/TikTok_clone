6d9c141ce4ab08d55840069c94460ac94ac4edd1
DESKTOP-EUE33NB\kmw36 <kmw365849@gmail.com>
2024-03-14 10:50:16 +0900

    TikTok Clone #3

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 
 import '../../../constants/sizes.dart';
 
@@ -19,16 +20,15 @@ class _VideoCommentsState extends State<VideoComments> {
   Widget build(BuildContext context) {
     return Container(
       clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(Sizes.size20)
-      ),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(Sizes.size20)),
       child: Scaffold(
         backgroundColor: Colors.grey.shade50,
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.grey.shade50,
           automaticallyImplyLeading: false,
-          title:Text('22822 Comments'),
+          title: Text('22822 Comments'),
           actions: [
             IconButton(
               onPressed: _onClosePressed,
@@ -38,9 +38,70 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(itemCount: 10,itemBuilder: (context,index) => Container(
-          child: Text('im a comment'),
-        )),
+        body: ListView.separated(
+          padding: EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
+          itemCount: 10,
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text('민'),
+              ),
+              Gaps.h10,
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '민우',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: Sizes.size14,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Gaps.v3,
+                  Text('Hello my name is Minwoo')
+                ],
+              )),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    '52.2K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text('민우'),
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
